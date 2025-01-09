90664bb24bbe0c9d58069be78de91f2dd17e9906
Elijah Massey <emassey0135@gmail.com>
2025-01-09 11:31:44 -0600

    Add database object for connecting and initializing the database

---
+++ src/main/kotlin/dev/emassey0135/audionavigation/Database.kt
@@ -0,0 +1,17 @@
+package dev.emassey0135.audionavigation
+
+import java.sql.Connection
+import java.sql.DriverManager
+import org.sqlite.SQLiteConfig
+
+object Database {
+  var connection: Connection
+  init {
+    val config = SQLiteConfig()
+    config.enableLoadExtension(true)
+    connection = DriverManager.getConnection("jdbc:sqlite:test.db", config.toProperties())
+    val statement = connection.createStatement()
+    statement.execute("SELECT load_extension('/usr/lib/mod_spatialite.so')")
+    statement.execute("SELECT InitSpatialMetadata(1)")
+  }
+}

6d96717d49a15b3e31ed1a42fc611fbbcea5286e
Elijah Massey <emassey0135@gmail.com>
2025-01-09 21:26:04 -0600

    Add function to write a Geometry to WKB as hex

--- src/main/kotlin/dev/emassey0135/audionavigation/Geometry.kt
+++ src/main/kotlin/dev/emassey0135/audionavigation/Geometry.kt
@@ -2,18 +2,24 @@ package dev.emassey0135.audionavigation
 
 import kotlin.math.round
 import org.locationtech.jts.geom.Coordinate
+import org.locationtech.jts.geom.Geometry
 import org.locationtech.jts.geom.GeometryFactory
 import org.locationtech.jts.geom.Point
 import org.locationtech.jts.geom.PrecisionModel
+import org.locationtech.jts.io.WKBWriter
 import net.minecraft.util.math.BlockPos
 
 object Geometry {
   val geometryFactory = GeometryFactory(PrecisionModel(), -1)
+  val wkbWriter = WKBWriter(3)
   fun pointToBlockPos(point: Point): BlockPos {
     val coord = point.getCoordinate()
     return BlockPos(round(coord.x).toInt(), round(coord.y).toInt(), round(coord.z).toInt())
   }
   fun blockPosToPoint(pos: BlockPos): Point {
     return geometryFactory.createPoint(Coordinate(pos.getX().toDouble(), pos.getY().toDouble(), pos.getZ().toDouble()))
+  }
+  fun geometryToWKBHex(geometry: Geometry): String {
+    return WKBWriter.toHex(wkbWriter.write(geometry))
   }
 }

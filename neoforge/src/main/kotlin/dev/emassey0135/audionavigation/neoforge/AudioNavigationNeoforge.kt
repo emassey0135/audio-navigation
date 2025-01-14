a2ca01ea6d3fcd460f9ba110527b887768f5710a
Elijah Massey <emassey0135@gmail.com>
2025-01-13 23:23:50 -0600

    Add client entrypoint for NeoForge

--- neoforge/src/main/kotlin/dev/emassey0135/audionavigation/neoforge/AudioNavigationNeoforge.kt
+++ neoforge/src/main/kotlin/dev/emassey0135/audionavigation/neoforge/AudioNavigationNeoforge.kt
@@ -1,8 +1,6 @@
 package dev.emassey0135.audionavigation.neoforge
 
 import net.neoforged.fml.common.Mod
-import thedarkcolour.kotlinforforge.forge.MOD_BUS
-import thedarkcolour.kotlinforforge.forge.MOD_CONTEXT
 import dev.emassey0135.audionavigation.AudioNavigation
 
 @Mod(AudioNavigation.MOD_ID) object AudioNavigationNeoforge {

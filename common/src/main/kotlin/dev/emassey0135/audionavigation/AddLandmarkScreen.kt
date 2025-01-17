a4fe8c2e1b096dc74ec0f5cfc74b64a039122649
Elijah Massey <emassey0135@gmail.com>
2025-01-17 09:33:33 -0600

    Fix cancel button on landmark screen

--- common/src/main/kotlin/dev/emassey0135/audionavigation/AddLandmarkScreen.kt
+++ common/src/main/kotlin/dev/emassey0135/audionavigation/AddLandmarkScreen.kt
@@ -4,12 +4,16 @@ import net.minecraft.client.gui.screen.Screen
 import net.minecraft.client.gui.tooltip.Tooltip
 import net.minecraft.client.gui.widget.ButtonWidget
 import net.minecraft.client.gui.widget.TextFieldWidget
+import net.minecraft.client.MinecraftClient
 import net.minecraft.text.Text
 import dev.emassey0135.audionavigation.AudioNavigation
 import dev.emassey0135.audionavigation.AudioNavigationClient
 
 class AddLandmarkScreen(val parent: Screen): Screen(Text.translatable("${AudioNavigation.MOD_ID}.screens.add_landmark")) {
   var landmarkName = ""
+  fun goUp() {
+    MinecraftClient.getInstance()?.setScreen(parent)
+  }
   override fun init() {
     val landmarkNameTextField = TextFieldWidget(textRenderer, 10, 10, width-20, 50, Text.translatable("${AudioNavigation.MOD_ID}.screens.add_landmark.landmark_name_text_field"))
     landmarkNameTextField.setChangedListener { text -> landmarkName = text }
@@ -17,7 +21,7 @@ class AddLandmarkScreen(val parent: Screen): Screen(Text.translatable("${AudioNa
     addDrawableChild(ButtonWidget.builder(Text.translatable("${AudioNavigation.MOD_ID}.screens.add_landmark.save_button"), { button -> AudioNavigationClient.addLandmark(landmarkName); close() })
       .dimensions(10, 70, 50, 20)
       .build())
-    addDrawableChild(ButtonWidget.builder(Text.translatable("${AudioNavigation.MOD_ID}.screens.add_landmark.cancel_button"), { button -> close() })
+    addDrawableChild(ButtonWidget.builder(Text.translatable("${AudioNavigation.MOD_ID}.screens.add_landmark.cancel_button"), { button -> goUp() })
       .dimensions(width/2+10, 70, 50, 20)
       .build())
   }

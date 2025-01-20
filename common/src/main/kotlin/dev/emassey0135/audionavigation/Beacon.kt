9ef936b69d4fba8ae50da399df989d8e7b375be1
Elijah Massey <emassey0135@gmail.com>
2025-01-19 20:31:05 -0600

    Add basic beacon support

---
+++ common/src/main/kotlin/dev/emassey0135/audionavigation/Beacon.kt
@@ -0,0 +1,55 @@
+package dev.emassey0135.audionavigation
+
+import java.lang.Thread
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.Optional
+import kotlin.concurrent.thread
+import org.lwjgl.openal.AL11
+import net.minecraft.client.MinecraftClient
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
+import dev.emassey0135.audionavigation.AudioNavigation
+import dev.emassey0135.audionavigation.Opus
+import dev.emassey0135.audionavigation.Poi
+import dev.emassey0135.audionavigation.SoundPlayer
+
+object Beacon {
+  private val beaconQueue = ArrayBlockingQueue<Optional<Poi>>(16)
+  fun initialize() {
+    SoundPlayer.addSource("beacon")
+    SoundPlayer.setSourceMaxDistance("beacon", 1.0f)
+    thread {
+      var currentBeacon: Optional<Poi> = Optional.empty()
+      var isPlaying = false
+      while (true) {
+        if (beaconQueue.peek()!=null)
+          currentBeacon = beaconQueue.poll()
+        if (currentBeacon.isPresent()) {
+          val minecraftClient = MinecraftClient.getInstance()
+          val player = minecraftClient.player
+          if (player!=null) {
+            val origin = BlockPos.ofFloored(player.getPos())
+            val orientation = player.getFacing()
+            SoundPlayer.setListenerPosition(origin, orientation)
+            SoundPlayer.setSourcePosition("beacon", currentBeacon.get().pos)
+            Opus.playOpusFromResource("beacon", "assets/${AudioNavigation.MOD_ID}/sounds/beacons/Classic_OffAxis.ogg")
+            isPlaying = true
+            while (isPlaying) {
+              Thread.sleep(10)
+              SoundPlayer.getSourceState("beacon", { state -> isPlaying = state==AL11.AL_PLAYING })
+            }
+          }
+        }
+        else {
+          Thread.sleep(10)
+        }
+      }
+    }
+  }
+  fun startBeacon(poi: Poi) {
+    beaconQueue.offer(Optional.of(poi))
+  }
+  fun stopBeacon() {
+    beaconQueue.offer(Optional.empty())
+  }
+}

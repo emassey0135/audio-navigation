615d7944ec9a07d3e8860cd421ba75f426667528
Elijah Massey <emassey0135@gmail.com>
2025-01-20 14:21:05 -0600

    Make ValidatedInt config fields use text boxes with buttons instead of sliders

--- common/src/main/kotlin/dev/emassey0135/audionavigation/ClientConfig.kt
+++ common/src/main/kotlin/dev/emassey0135/audionavigation/ClientConfig.kt
@@ -3,8 +3,9 @@ package dev.emassey0135.audionavigation
 import me.fzzyhmstrs.fzzy_config.config.Config
 import me.fzzyhmstrs.fzzy_config.config.ConfigSection
 import me.fzzyhmstrs.fzzy_config.validation.misc.ValidatedBoolean
-import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedInt
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedFloat
+import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedInt
+import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedNumber
 import net.minecraft.util.Identifier
 import dev.emassey0135.audionavigation.AudioNavigation
 import dev.emassey0135.audionavigation.SoundPlayer
@@ -13,30 +14,30 @@ import dev.emassey0135.audionavigation.Speech
 class ClientConfig: Config(Identifier.of(AudioNavigation.MOD_ID, "client_config")) {
   var announcements = AnnouncementsSection()
   class AnnouncementsSection: ConfigSection() {
-    var announcementRadius = ValidatedInt(25, 100, 1)
+    var announcementRadius = ValidatedInt(25, 100, 1, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS)
     var enableVerticalLimit = ValidatedBoolean(true)
-    var verticalLimit = ValidatedInt(5, 25, 1)
-    var maxAnnouncements = ValidatedInt(10, 25, 1)
+    var verticalLimit = ValidatedInt(5, 25, 1, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS)
+    var maxAnnouncements = ValidatedInt(10, 25, 1, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS)
     var detailedAnnouncements = ValidatedBoolean(true)
   }
   var manualAnnouncements = ManualAnnouncementsSection()
   class ManualAnnouncementsSection: ConfigSection() {
-    var announcementRadius = ValidatedInt(100, 100, 1)
+    var announcementRadius = ValidatedInt(100, 100, 1, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS)
     var enableVerticalLimit = ValidatedBoolean(true)
-    var verticalLimit = ValidatedInt(10, 25, 1)
-    var maxAnnouncements = ValidatedInt(25, 25, 1)
+    var verticalLimit = ValidatedInt(10, 25, 1, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS)
+    var maxAnnouncements = ValidatedInt(25, 25, 1, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS)
     var detailedAnnouncements = ValidatedBoolean(true)
   }
   var speech = SpeechSection()
   class SpeechSection: ConfigSection() {
-    var rate = ValidatedInt(175, 900, 80).also { it.listenToEntry { value -> if (Speech.isInitialized) Speech.setRate(value.get()) }}
-    var volume = ValidatedInt(200, 200, 0).also { it.listenToEntry { value -> if (Speech.isInitialized) Speech.setVolume(value.get()) }}
-    var pitch = ValidatedInt(50, 100, 0).also { it.listenToEntry { value -> if (Speech.isInitialized) Speech.setPitch(value.get()) }}
-    var pitchRange = ValidatedInt(50, 100, 0).also { it.listenToEntry { value -> if (Speech.isInitialized) Speech.setPitchRange(value.get()) }}
+    var rate = ValidatedInt(175, 900, 80, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS).also { it.listenToEntry { value -> if (Speech.isInitialized) Speech.setRate(value.get()) }}
+    var volume = ValidatedInt(200, 200, 0, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS).also { it.listenToEntry { value -> if (Speech.isInitialized) Speech.setVolume(value.get()) }}
+    var pitch = ValidatedInt(50, 100, 0, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS).also { it.listenToEntry { value -> if (Speech.isInitialized) Speech.setPitch(value.get()) }}
+    var pitchRange = ValidatedInt(50, 100, 0, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS).also { it.listenToEntry { value -> if (Speech.isInitialized) Speech.setPitchRange(value.get()) }}
   }
   var beacons = BeaconsSection()
   class BeaconsSection: ConfigSection() {
-    var maxOnAxisAngle = ValidatedInt(15, 90, 0)
+    var maxOnAxisAngle = ValidatedInt(15, 90, 0, ValidatedNumber.WidgetType.TEXTBOX_WITH_BUTTONS)
   }
   var sound = SoundSection()
   class SoundSection: ConfigSection() {

90e47b569341a2182a493c9b6b60fb5394b7aea3
Elijah Massey <emassey0135@gmail.com>
2025-03-01 18:54:17 -0600

    Change order of SampleFormat enum

--- common/src/main/kotlin/dev/emassey0135/audionavigation/speech/SpeechResult.kt
+++ common/src/main/kotlin/dev/emassey0135/audionavigation/speech/SpeechResult.kt
@@ -3,7 +3,7 @@ package dev.emassey0135.audionavigation.speech
 class SpeechResult(val pcm: ByteArray, sampleFormatByte: Byte, val sampleRate: Int) {
   val sampleFormat = SampleFormat.entries.get(sampleFormatByte.toInt())
   enum class SampleFormat {
-    F32,
-    S16
+    S16,
+    F32
   }
 }

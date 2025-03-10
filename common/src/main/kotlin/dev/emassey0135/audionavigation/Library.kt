666426438ad6195acbe346e76a95dc35f9610105
Elijah Massey <emassey0135@gmail.com>
2025-03-10 15:57:35 -0500

    Include the version of the Rust TTS library in its filename, and download eSpeak NG data again when the library is upgraded

--- common/src/main/kotlin/dev/emassey0135/audionavigation/Library.kt
+++ common/src/main/kotlin/dev/emassey0135/audionavigation/Library.kt
@@ -11,20 +11,23 @@ import dev.emassey0135.audionavigation.UnzipUtility
 object Library {
   private val IS_X86_64 = SystemUtils.OS_ARCH=="amd64" || SystemUtils.OS_ARCH=="x86_64"
   private val IS_AARCH64 = SystemUtils.OS_ARCH=="aarch64"
+  private val libraryVersion = "0.3.0"
+  private val baseLibraryName = "audio_navigation_tts_$libraryVersion"
   val libraryName = when {
-    SystemUtils.IS_OS_LINUX && IS_X86_64 -> "libaudio_navigation_tts_x86_64.so"
-    SystemUtils.IS_OS_LINUX && IS_AARCH64 -> "libaudio_navigation_tts_aarch64.so"
-    SystemUtils.IS_OS_WINDOWS && IS_X86_64 -> "audio_navigation_tts_x86_64.dll"
-    SystemUtils.IS_OS_WINDOWS && IS_AARCH64 -> "audio_navigation_tts_aarch64.dll"
-    SystemUtils.IS_OS_MAC && IS_X86_64 -> "libaudio_navigation_tts_x86_64.dylib"
-    SystemUtils.IS_OS_MAC && IS_AARCH64 -> "libaudio_navigation_tts_aarch64.dylib"
+    SystemUtils.IS_OS_LINUX && IS_X86_64 -> "lib${baseLibraryName}_x86_64.so"
+    SystemUtils.IS_OS_LINUX && IS_AARCH64 -> "lib${baseLibraryName}_aarch64.so"
+    SystemUtils.IS_OS_WINDOWS && IS_X86_64 -> "${baseLibraryName}_x86_64.dll"
+    SystemUtils.IS_OS_WINDOWS && IS_AARCH64 -> "${baseLibraryName}_aarch64.dll"
+    SystemUtils.IS_OS_MAC && IS_X86_64 -> "lib${baseLibraryName}_x86_64.dylib"
+    SystemUtils.IS_OS_MAC && IS_AARCH64 -> "lib${baseLibraryName}_aarch64.dylib"
     else -> error("The current operating system or CPU architecture is not supported by this mod.")
   }
   fun downloadLibrary() {
-    FileUtils.copyURLToFile(URI("https://github.com/emassey0135/audio-navigation-tts/releases/download/0.2.0/$libraryName").toURL(), File(libraryName))
+    FileUtils.copyURLToFile(URI("https://github.com/emassey0135/audio-navigation-tts/releases/download/$libraryVersion/$libraryName").toURL(), File(libraryName))
+    downloadAndExtractEspeakNGData()
   }
   fun downloadAndExtractEspeakNGData() {
-    FileUtils.copyURLToFile(URI("https://github.com/emassey0135/audio-navigation-tts/releases/download/0.2.0/espeak-ng-data.zip").toURL(), File("espeak-ng-data.zip"))
+    FileUtils.copyURLToFile(URI("https://github.com/emassey0135/audio-navigation-tts/releases/download/$libraryVersion/espeak-ng-data.zip").toURL(), File("espeak-ng-data.zip"))
     if (Files.exists(Paths.get("espeak-ng-data")))
       FileUtils.deleteDirectory(File("espeak-ng-data"))
     UnzipUtility().unzip("espeak-ng-data.zip", ".")

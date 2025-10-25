ef194412889a6df72c022c3a6ef79aa0065fe145
Elijah Massey <emassey0135@gmail.com>
2025-10-24 21:07:02 -0500

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
   java
-  kotlin("jvm") version "2.2.20"
-  kotlin("plugin.serialization") version "2.2.20" apply false
+  kotlin("jvm") version "2.2.21"
+  kotlin("plugin.serialization") version "2.2.21" apply false
   id("dev.architectury.loom") version "1.11-SNAPSHOT" apply false
   id("architectury-plugin") version "3.4-SNAPSHOT"
-  id("com.gradleup.shadow") version "9.1.0" apply false
-  id("io.papermc.paperweight.userdev") version "2.0.0-beta.18" apply false
+  id("com.gradleup.shadow") version "9.2.2" apply false
+  id("io.papermc.paperweight.userdev") version "2.0.0-beta.19" apply false
   id("io.github.dueris.eclipse.gradle") version "1.2.3" apply false
   id("com.modrinth.minotaur") version "2.+" apply false
   id("io.github.themrmilchmann.curseforge-publish") version "0.8.0" apply false
